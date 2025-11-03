703773ee0ec02a07197c853f696e710702e0b3b1
Amin Sedghi <sedghi.amin@gmail.com>
2025-11-03 10:01:13 +0330

    without the explicit dereference

--- Struct/example/app.go
+++ Struct/example/app.go
@@ -13,6 +13,6 @@ type Cars struct{
 func main(){
 	var car = &Cars{"Benz", "C200", "Black"}
 	fmt.Println(*car);
-	(*car).color = "blue"
-	fmt.Println("color's car changed to:", (*car).color)
+	car.color = "blue"
+	fmt.Println("color's car changed to:", car.color)
 }
