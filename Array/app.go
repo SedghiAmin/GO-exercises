b198ee74695e87a215569e3d38b86520c8e2ecb9
Amin Sedghi <sedghi.amin@gmail.com>
2025-11-03 11:39:54 +0330

    array example

---
+++ Array/app.go
@@ -0,0 +1,20 @@
+package main
+
+import(
+	"fmt"
+)
+
+func main(){ 
+
+	var arr [3]string 
+
+	arr[0] = "Ali"
+	arr[1] = "Amin"
+	arr[2] = "Ehsan"
+
+	fmt.Println(arr)
+
+	age := [3]int{65, 39, 38}
+
+	fmt.Println(age)
+}
