081f2a778d36140d8a40bf7da7822d1beb23707e
Amin Sedghi <sedghi.amin@gmail.com>
2025-11-19 12:55:49 +0330

    add Save method

--- GoWiki/wiki.go
+++ GoWiki/wiki.go
@@ -1,11 +1,17 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 )
 
 type Page struct{
 	Title string
-	Body []byte
+	Body []byte // that is the type expected by the io libraries we will use
 }
+
+func (p *Page) Save() error{	// returns an error value because that is the return type of WriteFile
+	filename:= p.Title + ".txt"
+	return os.WriteFile(filename, p.Body, 0600) //The octal integer literal 0600, passed as the third parameter to WriteFile, indicates that the file should be created with read-write permissions for the current user only.
+}
+
