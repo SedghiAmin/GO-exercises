87e85d8346c770c7e19f1d0f4bb7204cf0cca242
Amin Sedghi <sedghi.amin@gmail.com>
2025-11-25 13:59:47 +0330

    Error handling

--- GoWiki/wiki.go
+++ GoWiki/wiki.go
@@ -20,42 +20,67 @@ func (p *Page) Save() error { // returns an error value because that is the retu
 func loadPage(title string) (*Page, error) {
 	fileName := title + ".txt"
 	body, err := os.ReadFile(fileName) //The standard library function os.ReadFile returns []byte and error.
+
 	if err != nil {
 		return nil, err
 	}
+
 	return &Page{title, body}, nil
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
 	p, err := loadPage(title)
+
 	if err != nil {
 		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
 		return
 	}
+
 	renderTemplate(w, "view", p)
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/edit/"):]
 	p, err := loadPage(title)
+
 	if err != nil {
 		p = &Page{Title: title}
 	}
+
 	renderTemplate(w, "edit", p)
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/save/"):]
+	
 	body := r.FormValue("body")
+
 	p := &Page{Title: title, Body: []byte(body)}
-	p.Save()
+	err:= p.Save()
+
+	if err != nil{
+		http.Error(w, err.Error(), http.StatusInternalServerError )
+		return
+	}
+
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	t, _ := template.ParseFiles(tmpl + ".html")
-	t.Execute(w, p)
+	t, err := template.ParseFiles(tmpl + ".html")
+
+	if err != nil{
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return 
+	}
+
+	err = t.Execute(w, p)
+	
+	if err != nil{
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 func main() {
