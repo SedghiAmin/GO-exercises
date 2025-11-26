59661c66da711ca8e0d28b6c32af851b15033279
Amin Sedghi <sedghi.amin@gmail.com>
2025-11-26 11:23:06 +0330

    Template caching

--- GoWiki/wiki.go
+++ GoWiki/wiki.go
@@ -7,6 +7,9 @@ import (
 	"os"
 )
 
+//The function template.Must is a convenience wrapper that panics when passed a non-nil error value, and otherwise returns the *Template unaltered. 
+var templates = template.Must(template.ParseFiles("view.html", "edit.html"))
+
 type Page struct {
 	Title string
 	Body  []byte // that is the type expected by the io libraries we will use
@@ -68,18 +71,11 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	t, err := template.ParseFiles(tmpl + ".html")
+	err := templates.ExecuteTemplate(w, tmpl + ".html", p)
 
 	if err != nil{
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return 
-	}
-
-	err = t.Execute(w, p)
-	
-	if err != nil{
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
 	}
 }
 
