8d6ed2530cf81a315b61a57e5ba9fd47762af414
Amin Sedghi <sedghi.amin@gmail.com>
2025-11-29 11:54:04 +0330

    modify function name

--- Modules/Greetings/greating.go
+++ Modules/Greetings/greating.go
@@ -1,9 +1,10 @@
-package greetings
+package greetings //Declare greetings package to collect related functions.
 
 import(
 	"fmt"
 )
 
+//Implement Hello function to return the greeting
 func Hello (name string) string{
 	msg:= fmt.Sprintf("Hi %v. Welcome to Go!", name)
 	return msg
