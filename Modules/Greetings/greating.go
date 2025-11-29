ea740fdba3bd21954941c3dc953132e6a461a6ac
Amin Sedghi <sedghi.amin@gmail.com>
2025-11-29 13:06:13 +0330

    add error handling

--- Modules/Greetings/greating.go
+++ Modules/Greetings/greating.go
@@ -1,11 +1,18 @@
 package greetings //Declare greetings package to collect related functions.
 
-import(
+import (
+	"errors"
 	"fmt"
 )
 
 //Implement Hello function to return the greeting
-func Hello (name string) string{
+func Hello (name string) (string, error){
+
+	if name == ""{
+		return "", errors.New("empty name")
+	}
+
 	msg:= fmt.Sprintf("Hi %v. Welcome to Go!", name)
-	return msg
+
+	return msg, nil
 }
