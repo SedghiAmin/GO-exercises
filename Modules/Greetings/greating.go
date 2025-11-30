25088ec16b2dea840d5b95fd805c5ad51d911f2a
Amin Sedghi <sedghi.amin@gmail.com>
2025-11-30 12:35:42 +0330

    Return greetings for multiple people

--- Modules/Greetings/greating.go
+++ Modules/Greetings/greating.go
@@ -18,6 +18,19 @@ func Hello (name string) (string, error){
 	return msg, nil
 }
 
+func Hellos(names []string) (map[string]string, error){
+	messages:= make(map[string]string)
+
+	for _, name:= range names{
+		message, err:= Hello(name)
+		if err != nil{
+			return nil , err
+		}
+		messages[name]:= message
+	}
+	return messages , nil
+}
+
 func randomGreeting() string{
 	formats:= []string{
 		"Hi, %v. Welcome to GO!",
