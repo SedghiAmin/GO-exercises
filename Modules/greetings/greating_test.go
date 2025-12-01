840346b741e507d7ec105efcf402974056bb9ef6
Amin Sedghi <sedghi.amin@gmail.com>
2025-12-01 11:06:11 +0330

    progress greeting_test.go

--- Modules/greetings/greating_test.go
+++ Modules/greetings/greating_test.go
@@ -5,11 +5,18 @@ import(
 	"testing"
 )
 
-func TestHelloFunc(t *testing.T){
+func TestHelloAmin(t *testing.T){
 	name:= "Amin"
 	safeName:=  regexp.MustCompile(`\b` + name + `\b`)
 	msg, err:= Hello("Amin")
 	if !safeName.MatchString(msg) || err != nil{
 		t.Errorf(`Hello("Amin") = %q , %v , want match for %#q, nil`, msg, err, safeName)
+	}
+}
+
+func TestHelloEmpty(t *testing.T){
+	msg, err:= Hello("")
+	if msg != "" || err == nil{
+		t.Errorf(`Hello("") = %q, %v , want "", error`, msg, err)
 	}
 }
