84fea8998e22bc4e10c4dcdb0e9122fba20584e5
Amin Sedghi <sedghi.amin@gmail.com>
2025-12-08 14:45:54 +0330

    Define the CanSpy() function

--- Exercism/Annalyn's Infiltration/main.go
+++ Exercism/Annalyn's Infiltration/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 )
 
+// CanFastAttack can be executed only when the knight is sleeping.
 func CanFastAttack(knightIsAwake bool) bool {
 	if !knightIsAwake {
 		return true
@@ -11,7 +12,18 @@ func CanFastAttack(knightIsAwake bool) bool {
 	return false
 }
 
+// CanSpy can be executed if at least one of the characters is awake.
+func CanSpy(knightIsAwake, archerIsAwake, prisonerIsAwake bool) bool {
+	if knightIsAwake || archerIsAwake || prisonerIsAwake {
+		return true
+	}
+	return false
+}
+
 func main() {
-	var knightIsAwake = true
-	fmt.Println(CanFastAttack(knightIsAwake))
+	var knightIsAwake = false
+	var archerIsAwake = true
+	var prisonerIsAwake = false
+	fmt.Printf("Can Spy: %v\n", CanSpy(knightIsAwake, archerIsAwake, prisonerIsAwake))
+	fmt.Printf("Can Fast Attack: %v\n", CanFastAttack(knightIsAwake))
 }
