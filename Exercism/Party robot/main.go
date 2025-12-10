9e67aab1748131df2563dbb2b8d6474d119df0e3
Amin Sedghi <sedghi.amin@gmail.com>
2025-12-10 13:30:07 +0330

    add HappyBirthday function

--- Exercism/Party robot/main.go
+++ Exercism/Party robot/main.go
@@ -9,6 +9,13 @@ func Welcome(name string) string {
 	return fmt.Sprintf("Welcome to my party, %s!", name)
 }
 
+// HappyBirthday wishes happy birthday to the birthday person and exclaims their age.
+func HappyBirthday(name string, age int) string {
+	return fmt.Sprintf("Happy birthday %s! You are now %d years old!", name, age)
+}
+
 func main() {
 	fmt.Println(Welcome("Amin"))
+	fmt.Println(HappyBirthday("Amin", 39))
+
 }
