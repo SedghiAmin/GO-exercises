c1865d84a7d6af66dee148b2e3e841c36f43ccef
Amin Sedghi <sedghi.amin@gmail.com>
2025-12-13 13:10:50 +0330

    add Description func

--- Exercism/Booking up for Beauty/main.go
+++ Exercism/Booking up for Beauty/main.go
@@ -29,8 +29,22 @@ func IsAfternoonAppointment(date string) bool {
 	return false
 }
 
+// Description returns a formatted string of the appointment time.
+func Description(date string) string {
+	layout := "1/2/2006 15:04:05"
+	t, _ := time.Parse(layout, date)
+	weekDay := t.Weekday()
+	month := t.Month()
+	day := t.Day()
+	year := t.Year()
+	hour := t.Hour()
+	minute := t.Minute()
+	return fmt.Sprintf("You have an appointment on %v, %v %v, %v, at %v:%v.", weekDay, month, day, year, hour, minute)
+}
+
 func main() {
 	fmt.Println(Schedule("7/25/2019 13:45:00"))
 	fmt.Println(HasPassed("July 25, 2019 13:45:00"))
 	fmt.Println(IsAfternoonAppointment("Thursday, July 25, 2019 13:45:00"))
+	fmt.Println(Description("7/25/2019 13:45:00"))
 }
