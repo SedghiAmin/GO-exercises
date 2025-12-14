6c40ead3ba7d963b3c028fc9720a691b4cd76c38
Amin Sedghi <sedghi.amin@gmail.com>
2025-12-14 12:50:58 +0330

    CalculateCost

--- Exercism/Cars Assemble/main.go
+++ Exercism/Cars Assemble/main.go
@@ -16,7 +16,15 @@ func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int 
 	return int(float64(productionRate) * successRate / 100 / 60)
 }
 
+// CalculateCost works out the cost of producing the given number of cars.
+func CalculateCost(carsCount int) uint {
+	g := carsCount / 10 * 95000
+	i := carsCount % 10 * 10000
+	return uint(i + g)
+}
+
 func main() {
 	fmt.Println(CalculateWorkingCarsPerHour(1547, 90))
 	fmt.Println(CalculateWorkingCarsPerMinute(1105, 90))
+	fmt.Println(CalculateCost(21))
 }
