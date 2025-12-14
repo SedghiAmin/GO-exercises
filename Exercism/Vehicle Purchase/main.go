2feaeb4aac17f5b1047d135526076c7cfc0d7e7d
Amin Sedghi <sedghi.amin@gmail.com>
2025-12-14 14:24:28 +0330

    add ChooseVehicle func

--- Exercism/Vehicle Purchase/main.go
+++ Exercism/Vehicle Purchase/main.go
@@ -1,6 +1,8 @@
 package main
 
-import ()
+import (
+	"fmt"
+)
 
 // NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
 func NeedsLicense(kind string) bool {
@@ -8,6 +10,15 @@ func NeedsLicense(kind string) bool {
 		return true
 	}
 	return false
+}
+
+// ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
+func ChooseVehicle(option1, option2 string) string {
+	choice := option2
+	if option1 < option2 {
+		choice = option1
+	}
+	return fmt.Sprintf(`%s is clearly the better choice.`, choice)
 }
 
 func main() {
@@ -19,4 +30,10 @@ func main() {
 
 	println(NeedsLicense("truck"))
 	// => true
+
+	println(ChooseVehicle("Wuling Hongguang", "Toyota Corolla"))
+	// => "Toyota Corolla is clearly the better choice."
+
+	println(ChooseVehicle("Volkswagen Beetle", "Volkswagen Golf"))
+	// => "Volkswagen Beetle is clearly the better choice."
 }
