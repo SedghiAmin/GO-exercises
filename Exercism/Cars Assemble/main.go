2d58731ca10a58c68f633b63e3b010c4b8c60a26
Amin Sedghi <sedghi.amin@gmail.com>
2025-12-14 12:50:19 +0330

    add CalculateWorkingCarsPerMinute

--- Exercism/Cars Assemble/main.go
+++ Exercism/Cars Assemble/main.go
@@ -10,7 +10,13 @@ func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float6
 	return float64(productionRate) * successRate / 100
 }
 
+// CalculateWorkingCarsPerMinute calculates how many working cars are
+// produced by the assembly line every minute.
+func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
+	return int(float64(productionRate) * successRate / 100 / 60)
+}
+
 func main() {
 	fmt.Println(CalculateWorkingCarsPerHour(1547, 90))
-
+	fmt.Println(CalculateWorkingCarsPerMinute(1105, 90))
 }
