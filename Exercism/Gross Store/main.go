a6d5c4da586239254241bdebbb21c08cd80e0e49
Amin Sedghi <sedghi.amin@gmail.com>
2025-12-15 08:47:57 +0330

    units

---
+++ Exercism/Gross Store/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// Units stores the Gross Store unit measurements.
+func Units() map[string]int {
+	return map[string]int{
+		"quarter_of_a_dozen": 3,
+		"half_of_a_dozen":    6,
+		"dozen":              12,
+		"small_gross":        120,
+		"gross":              144,
+		"great_gross":        1728,
+	}
+}
+
+func main() {
+	units := Units()
+	fmt.Println(units)
+}
