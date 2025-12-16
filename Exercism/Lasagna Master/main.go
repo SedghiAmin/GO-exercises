adf79af15b0f5f0c714cbad2988f1e68e238b6f0
Amin Sedghi <sedghi.amin@gmail.com>
2025-12-16 10:01:11 +0330

    add Quantities func

--- Exercism/Lasagna Master/main.go
+++ Exercism/Lasagna Master/main.go
@@ -9,11 +9,24 @@ func PreparationTime(layers []string, prepartionTime int) int {
 	return len(layers) * 2
 }
 
+func Quantities(layers []string) (int, float64) {
+	var s, n int
+	for _, l := range layers {
+		if l == "sauce" {
+			s += 1
+		} else if l == "noodles" {
+			n += 1
+		}
+	}
+	return n * 50, float64(s) * 0.2
+}
+
 func main() {
 	layers := []string{"sauce", "noodles", "sauce", "meat", "mozzarella", "noodles"}
 	fmt.Println(PreparationTime(layers, 3))
 	// => 18
 	fmt.Println(PreparationTime(layers, 0))
 	// => 12
-
+	fmt.Println(Quantities(layers))
+	// => 100, 0.4
 }
