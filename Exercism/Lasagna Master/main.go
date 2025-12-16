f732469277b296fe275911c5546dd25a69450be0
Amin Sedghi <sedghi.amin@gmail.com>
2025-12-16 12:43:31 +0330

    ScaleRecipe

--- Exercism/Lasagna Master/main.go
+++ Exercism/Lasagna Master/main.go
@@ -25,6 +25,14 @@ func AddSecretIngredient(friendList []string, myList []string) {
 	myList[len(myList)-1] = friendList[len(myList)-1]
 }
 
+func ScaleRecipe(twoPortions []float64, portionsCount int) []float64 {
+	newPortions := make([]float64, 0, len(twoPortions))
+	for _, portion := range twoPortions {
+		newPortions = append(newPortions, float64(portionsCount)/2*portion)
+	}
+	return newPortions
+}
+
 func main() {
 	layers := []string{"sauce", "noodles", "sauce", "meat", "mozzarella", "noodles"}
 	fmt.Println(PreparationTime(layers, 3))
@@ -38,4 +46,7 @@ func main() {
 	AddSecretIngredient(friendsList, myList)
 	fmt.Printf("%#v\n", myList)
 	// myList => []string{"noodles", "meat", "sauce", "mozzarella", "kampot pepper"})
+	quantities := []float64{1.2, 3.6, 10.5}
+	fmt.Printf("%#v\n", ScaleRecipe(quantities, 4))
+	// => []float64{ 2.4, 7.2, 21 }
 }
