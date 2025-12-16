b49d7ffd29c7021b8a66910b2912c03d03946c7e
Amin Sedghi <sedghi.amin@gmail.com>
2025-12-16 15:16:19 +0330

    add NewCar func

--- Exercism/Need For Speed/main.go
+++ Exercism/Need For Speed/main.go
@@ -7,6 +7,16 @@ type Car struct {
 	distance     int
 }
 
+// NewCar creates a new remote controlled car with full battery and given specifications.
+func NewCar(speed, batteryDrain int) Car {
+	return Car{
+		battery:      100,
+		batteryDrain: batteryDrain,
+		speed:        speed,
+		distance:     0,
+	}
+}
+
 func main() {
 
 }
