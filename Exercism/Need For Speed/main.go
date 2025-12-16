07f529af2b77aa759d588cbd0c7e5fd53b157517
Amin Sedghi <sedghi.amin@gmail.com>
2025-12-16 15:22:30 +0330

    defining NewTrack func

--- Exercism/Need For Speed/main.go
+++ Exercism/Need For Speed/main.go
@@ -1,10 +1,16 @@
 package main
 
+import "fmt"
+
 type Car struct {
 	battery      int
 	batteryDrain int
 	speed        int
 	distance     int
+}
+
+type Track struct {
+	distance int
 }
 
 // NewCar creates a new remote controlled car with full battery and given specifications.
@@ -17,6 +23,17 @@ func NewCar(speed, batteryDrain int) Car {
 	}
 }
 
-func main() {
+// NewTrack creates a new track
+func NewTrack(distance int) Track {
+	return Track{distance: distance}
+}
 
+func main() {
+	speed := 5
+	batteryDrain := 2
+	car := NewCar(speed, batteryDrain)
+	fmt.Printf("%#v\n", car)
+	distance := 800
+	track := NewTrack(distance)
+	fmt.Printf("%#v\n", track)
 }
