da12599feb2377ed8e9cd87e42a6a55256817a83
Amin Sedghi <sedghi.amin@gmail.com>
2025-12-20 08:53:30 +0330

    add GetItem func

--- Exercism/Card Tricks/main.go
+++ Exercism/Card Tricks/main.go
@@ -7,6 +7,16 @@ func FavoriteCards() []int {
 	return []int{2, 6, 9}
 }
 
+// GetItem retrieves an item from a slice at given position.
+// If the index is out of range, we want it to return -1.
+func GetItem(slice []int, index int) int {
+	if index >= len(slice) {
+		return -1 //If the index is out of bounds (ie. if it is negative or after the end of the stack), return -1
+	}
+	return slice[index]
+}
+
 func main() {
-	fmt.Printf("%#v", FavoriteCards())
+	fmt.Printf("%#v\n", FavoriteCards())
+	fmt.Printf("%#v\n", GetItem([]int{1, 2, 4, 1}, 10))
 }
