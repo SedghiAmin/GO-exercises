2928f25875d74fbea24d10111cccf9b72434d857
Amin Sedghi <sedghi.amin@gmail.com>
2025-12-20 09:15:28 +0330

    add RemoveItem func

--- Exercism/Card Tricks/main.go
+++ Exercism/Card Tricks/main.go
@@ -34,9 +34,18 @@ func PrependItems(slice []int, values ...int) []int {
 	return slice
 }
 
+// RemoveItem removes an item from a slice by modifying the existing slice.
+func RemoveItem(slice []int, index int) []int {
+	if index >= len(slice) || index < 0 {
+		return slice
+	}
+	return append(slice[:index], slice[index+1:]...)
+}
+
 func main() {
 	fmt.Printf("%#v\n", FavoriteCards())
 	fmt.Printf("%#v\n", GetItem([]int{1, 2, 4, 1}, 10))
 	fmt.Printf("%#v\n", SetItem([]int{1, 2, 4, 1}, -1, 6))
 	fmt.Printf("%#v\n", PrependItems([]int{3, 2, 6, 4, 8}, 5, 1))
+	fmt.Printf("%#v\n", RemoveItem([]int{3, 2, 6, 4, 8}, 2))
 }
