090462b89142ae5cbd198ec1edf35f2348ac9d51
Amin Sedghi <sedghi.amin@gmail.com>
2025-12-20 09:00:25 +0330

    add SetItem func

--- Exercism/Card Tricks/main.go
+++ Exercism/Card Tricks/main.go
@@ -16,7 +16,18 @@ func GetItem(slice []int, index int) int {
 	return slice[index]
 }
 
+// SetItem writes an item to a slice at given position overwriting an existing value.
+// If the index is out of range the value needs to be appended.
+func SetItem(slice []int, index, value int) []int {
+	if index >= len(slice) || index < 0 {
+		return append(slice, value)
+	}
+	slice[index] = value
+	return slice
+}
+
 func main() {
 	fmt.Printf("%#v\n", FavoriteCards())
 	fmt.Printf("%#v\n", GetItem([]int{1, 2, 4, 1}, 10))
+	fmt.Printf("%#v\n", SetItem([]int{1, 2, 4, 1}, -1, 6))
 }
