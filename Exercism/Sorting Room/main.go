31cf1b649d80d8961a7575d1a9c5a66a1d4a8314
Amin Sedghi <sedghi.amin@gmail.com>
2025-12-21 14:48:32 +0330

    DescribeNumberBox

--- Exercism/Sorting Room/main.go
+++ Exercism/Sorting Room/main.go
@@ -2,10 +2,28 @@ package main
 
 import "fmt"
 
+type NumberBox interface {
+	Number() int
+}
+
+type numberBoxContaining struct {
+	number int
+}
+
+func (box numberBoxContaining) Number() int {
+	return box.number
+}
+
 func DescribeNumber(f float64) string {
 	return fmt.Sprintf("This is the number %.1f", f)
 }
 
+// DescribeNumberBox should return a string describing the NumberBox.
+func DescribeNumberBox(nb NumberBox) string {
+	return fmt.Sprintf("This is a box containing the number %.1f", float64(nb.Number()))
+}
+
 func main() {
-	fmt.Printf(DescribeNumber(-12.345))
+	fmt.Println(DescribeNumber(-12.345))
+	fmt.Println(DescribeNumberBox(numberBoxContaining{12}))
 }
