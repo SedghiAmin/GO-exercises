b46a00734a7aa7a7950ddacdb850bedc72c53c4c
Amin Sedghi <sedghi.amin@gmail.com>
2025-12-24 11:11:16 +0330

    create fetch card

--- Blackjack 21/main.go
+++ Blackjack 21/main.go
@@ -6,6 +6,25 @@ import (
 	"time"
 )
 
+func FetchCard(rng *rand.Rand, suits []string, drawnCards map[string]bool) string {
+	l := true
+	var fetch string = ""
+	for l {
+		cardNum := rng.Intn(13) + 1 // 1-13
+		suitIndex := rng.Intn(4)    // 0-3
+		deck := rng.Intn(6) + 1
+
+		key := fmt.Sprintf("%d-%d-%s", deck, cardNum, suits[suitIndex])
+
+		if !drawnCards[key] {
+			drawnCards[key] = true
+			fetch = fmt.Sprintf("Deck %d: %s %d", deck, suits[suitIndex], cardNum)
+			l = false
+		}
+	}
+	return fetch
+}
+
 func main() {
 	// create generator
 	source := rand.NewSource(time.Now().UnixNano())
@@ -18,17 +37,7 @@ func main() {
 	drawnCards := make(map[string]bool)
 
 	fmt.Println("Random Cards: ")
-	for i := 0; i < cardsPerDeck*numberOfDecks; {
-		cardNum := rng.Intn(13) + 1 // 1-13
-		suitIndex := rng.Intn(4)    // 0-3
-		deck := rng.Intn(6) + 1
-
-		key := fmt.Sprintf("%d-%d-%s", deck, cardNum, suits[suitIndex])
-
-		if !drawnCards[key] {
-			drawnCards[key] = true
-			fmt.Printf("Deck %d: %s %d\n", deck, suits[suitIndex], cardNum)
-			i++
-		}
+	for i := 0; i < cardsPerDeck*numberOfDecks; i++ {
+		fmt.Printf("%d: %s\n", i+1, FetchCard(rng, suits, drawnCards))
 	}
 }
