2631966e63a74fde3c120b8080cb74e01358a59f
Amin Sedghi <sedghi.amin@gmail.com>
2025-12-28 09:58:20 +0330

    implement newresident func

--- Exercism/Census/main.go
+++ Exercism/Census/main.go
@@ -6,3 +6,8 @@ type Resident struct {
 	Age     int
 	Address map[string]string
 }
+
+// NewResident registers a new resident in this city.
+func NewResident(name string, age int, address map[string]string) *Resident {
+	return &Resident{name, age, address}
+}
