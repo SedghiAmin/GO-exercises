57a2feb6d7fdd5689ed353ec4ed580bc80dab65b
Amin Sedghi <sedghi.amin@gmail.com>
2025-12-28 10:03:01 +0330

    implement main func

--- Exercism/Census/main.go
+++ Exercism/Census/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // Resident represents a resident in this city.
 type Resident struct {
 	Name    string
@@ -10,4 +12,13 @@ type Resident struct {
 // NewResident registers a new resident in this city.
 func NewResident(name string, age int, address map[string]string) *Resident {
 	return &Resident{name, age, address}
+}
+
+func main() {
+	name := "Matthew Sanabria"
+	age := 29
+	address := map[string]string{"street": "Main St."}
+
+	fmt.Println(NewResident(name, age, address))
+	// => &{Matthew Sanabria 29 map[street:Main St.]}
 }
