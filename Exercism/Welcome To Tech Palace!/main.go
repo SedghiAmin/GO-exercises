8bdc80e2a4aa2800aab8dbfa01efdbb582dfb694
Amin Sedghi <sedghi.amin@gmail.com>
2025-12-28 13:24:27 +0330

    Implement CleanupMessage func

--- Exercism/Welcome To Tech Palace!/main.go
+++ Exercism/Welcome To Tech Palace!/main.go
@@ -10,7 +10,27 @@ func WelcomeMessage(customer string) string {
 	return " Welcome to the Tech Palace, " + strings.ToUpper(customer)
 }
 
+// AddBorder adds a border to a welcome message.
+func AddBorder(welcomeMsg string, numStarsPerLine int) string {
+	return strings.Repeat("*", numStarsPerLine) + "\n" + welcomeMsg + "\n" + strings.Repeat("*", numStarsPerLine)
+}
+
+// CleanupMessage cleans up an old marketing message.
+func CleanupMessage(oldMsg string) string {
+	msg := strings.ReplaceAll(oldMsg, "*", "")
+	msg = strings.TrimSpace(msg)
+	return msg
+}
+
 func main() {
 	fmt.Println(WelcomeMessage("Judy"))
 	// => Welcome to the Tech Palace, JUDY
+
+	fmt.Println(AddBorder("Welcome!", 10))
+	/* => output:
+	***********
+	Welcome!
+	***********
+	*/
+
 }
