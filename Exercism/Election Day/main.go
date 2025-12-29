48f6cb909cf23dc1ec372f2f0321ac9dfffdcc2e
Amin Sedghi <sedghi.amin@gmail.com>
2025-12-29 11:08:05 +0330

    implement main func

--- Exercism/Election Day/main.go
+++ Exercism/Election Day/main.go
@@ -1,7 +1,18 @@
 package main
 
+import "fmt"
+
 // NewVoteCounter returns a new vote counter with
 // a given number of initial votes.
 func NewVoteCounter(initialVotes int) *int {
 	return &initialVotes
+}
+
+func main() {
+	var initialVotes int
+	initialVotes = 2
+
+	var counter *int
+	counter = NewVoteCounter(initialVotes)
+	fmt.Println(*counter == initialVotes) // true
 }
