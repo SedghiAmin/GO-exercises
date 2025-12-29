a16737e7c5b048e1b7a4fb770cad2d97934d8ec6
Amin Sedghi <sedghi.amin@gmail.com>
2025-12-29 11:08:34 +0330

    VoteCount func

--- Exercism/Election Day/main.go
+++ Exercism/Election Day/main.go
@@ -8,6 +8,11 @@ func NewVoteCounter(initialVotes int) *int {
 	return &initialVotes
 }
 
+// VoteCount extracts the number of votes from a counter.
+func VoteCount(counter *int) int {
+	return *counter
+}
+
 func main() {
 	var initialVotes int
 	initialVotes = 2
