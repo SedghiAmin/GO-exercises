89b506c282fc73d6b54041bc9fc15a709a7e4262
Amin Sedghi <sedghi.amin@gmail.com>
2025-12-29 12:36:35 +0330

    InterestRate func

---
+++ Exercism/Interest is Interesting/main.go
@@ -0,0 +1,14 @@
+package main
+
+// InterestRate returns the interest rate for the provided balance.
+func InterestRate(balance float64) float32 {
+	switch {
+	case balance >= 0 && balance < 1000:
+		return 0.5
+	case balance >= 1000 && balance < 5000:
+		return 1.621
+	case balance >= 5000:
+		return 2.475
+	}
+	return 3.213
+}
