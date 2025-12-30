2e1c729e244c07f04a3bf4d9c08cbd7e72b5ec80
Amin Sedghi <sedghi.amin@gmail.com>
2025-12-30 14:45:03 +0330

    implement ShuffleAnimals func

--- Exercism/Animal Magic/main.go
+++ Exercism/Animal Magic/main.go
@@ -16,6 +16,25 @@ func GenerateWandEnergy() float64 {
 	return rand.Float64() * 12
 }
 
+// ShuffleAnimals returns a slice with all eight animal strings in random order.
+func ShuffleAnimals() []string {
+	rand.Seed(time.Now().UnixNano())
+	animals := []string{
+		"ant",
+		"beaver",
+		"cat",
+		"dog",
+		"elephant",
+		"fox",
+		"giraffe",
+		"hedgehog",
+	}
+	rand.Shuffle(len(animals), func(i, j int) {
+		animals[i], animals[j] = animals[j], animals[i]
+	})
+	return animals
+}
+
 func main() {
 	fmt.Println(RollADie())           // d will be assigned a random int, 1 <= d <= 20
 	fmt.Println(GenerateWandEnergy()) // f will be assigned a random float64, 0.0 <= f < 12.0
