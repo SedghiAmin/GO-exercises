dd84e9fb08c54e6da1f7c081de577c4b656131b6
Amin Sedghi <sedghi.amin@gmail.com>
2025-12-30 14:17:20 +0330

    implement main func

--- Exercism/Animal Magic/main.go
+++ Exercism/Animal Magic/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 )
 
 // RollADie returns a random int d with 1 <= d <= 20.
 func RollADie() int {
 	return rand.Intn(20) + 1
+}
+
+func main() {
+	fmt.Println(RollADie()) // d will be assigned a random int, 1 <= d <= 20
 }
