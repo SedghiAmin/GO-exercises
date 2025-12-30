f38adc89cf1eb95e08e03b550c0166d08c2e77bf
Amin Sedghi <sedghi.amin@gmail.com>
2025-12-30 14:44:17 +0330

    implement GenerateWandEnergy func

--- Exercism/Animal Magic/main.go
+++ Exercism/Animal Magic/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 // RollADie returns a random int d with 1 <= d <= 20.
@@ -10,6 +11,13 @@ func RollADie() int {
 	return rand.Intn(20) + 1
 }
 
+// GenerateWandEnergy returns a random float64 f with 0.0 <= f < 12.0.
+func GenerateWandEnergy() float64 {
+	return rand.Float64() * 12
+}
+
 func main() {
-	fmt.Println(RollADie()) // d will be assigned a random int, 1 <= d <= 20
+	fmt.Println(RollADie())           // d will be assigned a random int, 1 <= d <= 20
+	fmt.Println(GenerateWandEnergy()) // f will be assigned a random float64, 0.0 <= f < 12.0
+	fmt.Println(ShuffleAnimals())
 }
