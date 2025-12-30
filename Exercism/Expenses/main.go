c9231217ae962d5fe53c5d776e74a9f7c94080ca
Amin Sedghi <sedghi.amin@gmail.com>
2025-12-30 10:28:13 +0330

    Filter

--- Exercism/Expenses/main.go
+++ Exercism/Expenses/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // Record represents an expense record.
 type Record struct {
 	Day      int
@@ -16,4 +18,15 @@ type DaysPeriod struct {
 // Day1Records only returns true for records that are from day 1
 func Day1Records(r Record) bool {
 	return r.Day == 1
+}
+
+// Filter returns the records for which the predicate function returns true.
+func Filter(in []Record, predicate func(Record) bool) []Record {
+	out := make([]Record, 0)
+	for _, record := range in {
+		if predicate(record) {
+			out = append(out, record)
+		}
+	}
+	return out
 }
