dabed073b1135db3d6966fe8775cc494e2ad1955
Amin Sedghi <sedghi.amin@gmail.com>
2026-01-04 14:38:55 +0330

    implement main func

--- Exercism/Parsing Log Files/main.go
+++ Exercism/Parsing Log Files/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"regexp"
 )
 
 func IsValidLine(text string) bool {
 	re := regexp.MustCompile(`^\[(TRC|DBG|INF|WRN|ERR|FTL)\].*`)
 	return re.MatchString(text)
+}
+
+func main() {
+	fmt.Println(IsValidLine("[ERR] A good error here"))
 }
