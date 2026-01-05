9e6af0b5d3e997889bc6d5c85a4f5c1426985d6f
Amin Sedghi <sedghi.amin@gmail.com>
2026-01-05 11:35:19 +0330

    defined types and constants

---
+++ Exercism/Meteorology/main.go
@@ -0,0 +1,41 @@
+package main
+
+type TemperatureUnit int
+
+const (
+	Celsius    TemperatureUnit = 0
+	Fahrenheit TemperatureUnit = 1
+)
+
+// Add a String method to the TemperatureUnit type
+
+type Temperature struct {
+	degree int
+	unit   TemperatureUnit
+}
+
+// Add a String method to the Temperature type
+
+type SpeedUnit int
+
+const (
+	KmPerHour    SpeedUnit = 0
+	MilesPerHour SpeedUnit = 1
+)
+
+// Add a String method to SpeedUnit
+
+type Speed struct {
+	magnitude int
+	unit      SpeedUnit
+}
+
+// Add a String method to Speed
+
+type MeteorologyData struct {
+	location      string
+	temperature   Temperature
+	windDirection string
+	windSpeed     Speed
+	humidity      int
+}
