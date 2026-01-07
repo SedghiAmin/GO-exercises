9b136241b99b8503aafdd9454d7e594c6943b68e
Amin Sedghi <sedghi.amin@gmail.com>
2026-01-07 12:01:55 +0330

    implement Difference of Squares

---
+++ Exercism/Difference of Squares/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func SquareOfSum(n int) int {
+	sum := 0
+	if n < 1 {
+		return sum
+	}
+	sum = n * (n + 1) / 2
+	return sum * sum
+}
+
+func SumOfSquares(n int) int {
+	if n < 1 {
+		return 0
+	}
+	return n * (n + 1) * (2*n + 1) / 6
+}
+
+func Difference(n int) int {
+	return SquareOfSum(n) - SumOfSquares(n)
+}
+
+func main() {
+	fmt.Println(Difference(10))
+}
