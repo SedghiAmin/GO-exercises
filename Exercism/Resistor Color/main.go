4248ecedd6ce43572b72364793516bdfd9db28d5
Amin Sedghi <sedghi.amin@gmail.com>
2026-01-27 16:03:15 +0330

    implement Resistor Color

--- Exercism/Resistor Color/main.go
+++ Exercism/Resistor Color/main.go
@@ -1,1 +1,42 @@
-package Resistor_Color
+package main
+
+import "fmt"
+
+var colorCodes = map[string]int{
+	"black":  0,
+	"brown":  1,
+	"red":    2,
+	"orange": 3,
+	"yellow": 4,
+	"green":  5,
+	"blue":   6,
+	"violet": 7,
+	"grey":   8,
+	"white":  9,
+}
+
+// Colors returns the list of all colors.
+func Colors() []string {
+	return []string{
+		"black",
+		"brown",
+		"red",
+		"orange",
+		"yellow",
+		"green",
+		"blue",
+		"violet",
+		"grey",
+		"white",
+	}
+}
+
+// ColorCode returns the resistance value of the given color.
+func ColorCode(color string) int {
+	return colorCodes[color]
+}
+
+func main() {
+	fmt.Println(Colors())
+	fmt.Println(ColorCode("black"))
+}
