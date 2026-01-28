9c1bcded7a7de01d1163b29d940ecb411ba7db31
Amin Sedghi <sedghi.amin@gmail.com>
2026-01-28 14:09:57 +0330

    implement torna func

---
+++ Exercism/RNA Transcription/main.go
@@ -0,0 +1,20 @@
+package main
+
+func ToRNA(dna string) string {
+	rna := make([]byte, len(dna))
+	for i := 0; i < len(dna); i++ {
+		switch dna[i] {
+		case 'G':
+			rna[i] = 'C'
+		case 'C':
+			rna[i] = 'G'
+		case 'T':
+			rna[i] = 'A'
+		case 'A':
+			rna[i] = 'U'
+		default:
+			rna[i] = dna[i]
+		}
+	}
+	return string(rna)
+}
