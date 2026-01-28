06986b17624828705a4cfe0cb90eb8368f5bb368
Amin Sedghi <sedghi.amin@gmail.com>
2026-01-28 14:12:22 +0330

    implement main func

--- Exercism/RNA Transcription/main.go
+++ Exercism/RNA Transcription/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func ToRNA(dna string) string {
 	rna := make([]byte, len(dna))
 	for i := 0; i < len(dna); i++ {
@@ -17,4 +19,22 @@ func ToRNA(dna string) string {
 		}
 	}
 	return string(rna)
+}
+
+func main() {
+
+	dna1 := "GCTA"
+	rna1 := ToRNA(dna1)
+	fmt.Printf("DNA: %s -> RNA: %s\n", dna1, rna1)
+	// output: DNA: GCTA -> RNA: CGAU
+
+	dna2 := "ACGTGGTCTTAA"
+	rna2 := ToRNA(dna2)
+	fmt.Printf("DNA: %s -> RNA: %s\n", dna2, rna2)
+	// output: DNA: ACGTGGTCTTAA -> RNA: UGCACCAGAAUU
+
+	dna3 := "GCTAX"
+	rna3 := ToRNA(dna3)
+	fmt.Printf("DNA: %s -> RNA: %s\n", dna3, rna3)
+	// output: DNA: GCTAX -> RNA: CGAUX
 }
