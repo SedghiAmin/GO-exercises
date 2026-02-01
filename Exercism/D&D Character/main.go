2d1652e12e57b5aab8512557028af76e4b63ebc2
Amin Sedghi <sedghi.amin@gmail.com>
2026-02-01 13:08:37 +0330

    implement Modifier func

--- Exercism/D&D Character/main.go
+++ Exercism/D&D Character/main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"math"
+)
+
 type Character struct {
 	Strength     int
 	Dexterity    int
@@ -8,4 +12,9 @@ type Character struct {
 	Wisdom       int
 	Charisma     int
 	Hitpoints    int
+}
+
+// Modifier calculates the ability modifier for a given ability score
+func Modifier(score int) int {
+	return int(math.Floor(float64(score-10) / 2))
 }
