3982389686a376e71cc30cf5fdbcf4398fcd229c
Amin Sedghi <sedghi.amin@gmail.com>
2026-02-01 13:09:21 +0330

    implement Ability func

--- Exercism/D&D Character/main.go
+++ Exercism/D&D Character/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"math"
+	"math/rand"
 )
 
 type Character struct {
@@ -17,4 +18,24 @@ type Character struct {
 // Modifier calculates the ability modifier for a given ability score
 func Modifier(score int) int {
 	return int(math.Floor(float64(score-10) / 2))
+}
+
+// Ability uses randomness to generate the score for an ability
+func Ability() int {
+
+	sum := 0
+	dice := make([]int, 4)
+
+	dice[0] = rand.Intn(6) + 1
+	for j := 1; j < 4; j++ {
+		dice[j] = rand.Intn(5) + 1
+		if dice[j] < dice[0] {
+			tmp := dice[j]
+			dice[j] = dice[0]
+			dice[0] = tmp
+		}
+		sum += dice[j]
+	}
+
+	return sum
 }
