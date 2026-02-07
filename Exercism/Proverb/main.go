e3681eec1b4b10ed4233830ae9ce95e853899a26
Amin Sedghi <sedghi.amin@gmail.com>
2026-02-07 13:15:46 +0330

    implement main func

--- Exercism/Proverb/main.go
+++ Exercism/Proverb/main.go
@@ -1,1 +1,23 @@
-package Proverb
+package main
+
+import "fmt"
+
+func Proverb(rhyme []string) []string {
+	if len(rhyme) == 0 {
+		return rhyme
+	}
+	r := make([]string, len(rhyme))
+	for i := 0; i < len(rhyme)-1; i++ {
+		r[i] = "For want of a " + rhyme[i] + " the " + rhyme[i+1] + " was lost."
+	}
+	r[len(rhyme)-1] = "And all for the want of a " + rhyme[0] + "."
+	return r
+}
+
+func main() {
+	result := Proverb([]string{"nail", "shoe", "horse"})
+
+	for i, line := range result {
+		fmt.Printf("Line %d: %s\n", i+1, line)
+	}
+}
