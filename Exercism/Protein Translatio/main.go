057ee8aefe9207fe09117e74f5dcd8c2321ce391
Amin Sedghi <sedghi.amin@gmail.com>
2026-02-07 15:51:36 +0330

    define FromRNA func

--- Exercism/Protein Translatio/main.go
+++ Exercism/Protein Translatio/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"fmt"
 )
 
 var (
@@ -27,4 +28,27 @@ var acid map[string]string = map[string]string{
 	"UAA": "STOP",
 	"UAG": "STOP",
 	"UGA": "STOP",
+}
+
+func FromRNA(rna string) ([]string, error) {
+	if len(rna)%3 != 0 {
+		return nil, ErrInvalidBase
+	}
+	r := []string{}
+	str := ""
+	for i, char := range rna {
+		str += string(char)
+		if (i+1)%3 == 0 {
+			protein, err := FromCodon(str)
+			if err != nil {
+				if err == ErrStop {
+					return r, nil
+				}
+				return r, err
+			}
+			r = append(r, protein)
+			str = ""
+		}
+	}
+	return r, nil
 }
