81b8c198cca6f8a22229e8eea4fd64f51ac0a9c3
Amin Sedghi <sedghi.amin@gmail.com>
2026-02-07 12:04:54 +0330

    implement KindFromSides func

--- Exercism/Triangle/main.go
+++ Exercism/Triangle/main.go
@@ -8,3 +8,28 @@ const (
 	Iso
 	Sca
 )
+
+func KindFromSides(a, b, c float64) Kind {
+	//var k Kind
+	if (a+b < c || b+c < a || a+c < b) || (a <= 0 || b <= 0 || c <= 0) {
+		return NaT
+	} else {
+		z := 1
+		if a == b {
+			z++
+		}
+		if b == c {
+			z++
+		}
+		if a == c {
+			z++
+		}
+		if z >= 3 {
+			return Equ
+		}
+		if z >= 2 {
+			return Iso
+		}
+	}
+	return Sca
+}
