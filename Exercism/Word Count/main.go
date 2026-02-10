28a2b9ce0be2fda6f8391ac2ff1a1b4f2dadb18d
Amin Sedghi <sedghi.amin@gmail.com>
2026-02-10 12:00:59 +0330

    implement main func for word count project

--- Exercism/Word Count/main.go
+++ Exercism/Word Count/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"regexp"
 	"strings"
 )
@@ -15,4 +16,26 @@ func WordCount(phrase string) Frequency {
 		result[word]++
 	}
 	return result
+}
+
+func main() {
+	testCases := []struct {
+		name string
+		text string
+	}{
+		{"simple", "Hello World"},
+		{"punctuations", "Hello, World! How are you?"},
+		{"contractions", "I don't know what you're saying"},
+		{"numbers", "I have 2 apples and 3 bananas"},
+		{"all", "That's 100% correct! Don't you agree?"},
+	}
+
+	for _, tc := range testCases {
+		words := WordCount(tc.text)
+
+		fmt.Printf("\n%s:\n", tc.name)
+		fmt.Printf("Text: %q\n", tc.text)
+		fmt.Printf("Words: %v\n", words)
+		fmt.Printf("Counts: %d\n", len(words))
+	}
 }
