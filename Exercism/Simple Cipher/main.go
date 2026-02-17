145d3f0b0d9c079b6401646b90e147a8866013e6
Amin Sedghi <sedghi.amin@gmail.com>
2026-02-17 13:34:59 +0330

    define Encode & Decode methods for shift

--- Exercism/Simple Cipher/main.go
+++ Exercism/Simple Cipher/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"strings"
 	"unicode"
 )
@@ -23,4 +22,46 @@ func NewShift(distance int) Cipher {
 		return nil
 	}
 	return shift(distance)
+}
+
+func (c shift) Encode(input string) string {
+	var output strings.Builder
+	shiftValue := int(c)
+
+	for _, chr := range input {
+		if unicode.IsLetter(chr) {
+			lower := unicode.ToLower(chr)
+
+			encoded := int(lower-'a') + shiftValue
+
+			encoded %= 26
+			if encoded < 0 {
+				encoded += 26
+			}
+
+			output.WriteRune(rune(encoded) + 'a')
+		}
+	}
+	return output.String()
+}
+
+func (c shift) Decode(input string) string {
+	var output strings.Builder
+	shiftValue := int(c)
+
+	for _, chr := range input {
+		if unicode.IsLetter(chr) {
+			lower := unicode.ToLower(chr)
+
+			decoded := int(lower-'a') - shiftValue
+
+			decoded %= 26
+			if decoded < 0 {
+				decoded += 26
+			}
+
+			output.WriteRune(rune(decoded) + 'a')
+		}
+	}
+	return output.String()
 }
