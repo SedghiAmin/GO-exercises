03f7b8634023d4d5918e455e81f711dd55b51577
Amin Sedghi <sedghi.amin@gmail.com>
2026-02-17 13:33:43 +0330

    define NewCaesar & NewShift func

--- Exercism/Simple Cipher/main.go
+++ Exercism/Simple Cipher/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+
 type Cipher interface {
 	Encode(string) string
 	Decode(string) string
@@ -7,3 +13,14 @@ type Cipher interface {
 
 type shift int
 type vigenere string
+
+func NewCaesar() Cipher {
+	return NewShift(3)
+}
+
+func NewShift(distance int) Cipher {
+	if distance < -25 || distance > 25 || distance == 0 {
+		return nil
+	}
+	return shift(distance)
+}
