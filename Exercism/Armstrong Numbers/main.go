6597f5884159909c0a374709ebeb642dc81a0046
Amin Sedghi <sedghi.amin@gmail.com>
2026-02-18 10:16:15 +0330

    implement Armstrong Numbers exercise

---
+++ Exercism/Armstrong Numbers/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func IsNumber(n int) bool {
+	if n == 0 {
+		return true
+	}
+	if n < 0 {
+		n = -n
+	}
+	digits := make([]int, 0)
+	number := n
+	for n > 0 {
+		digits = append(digits, n%10)
+		n /= 10
+	}
+	sum := 0.0
+	for i := range digits {
+		sum += math.Pow(float64(digits[i]), float64(len(digits)))
+	}
+	if sum == float64(number) {
+		return true
+	}
+	return false
+
+}
+func main() {
+	fmt.Println(IsNumber(10))
+	fmt.Println(IsNumber(9))
+	fmt.Println(IsNumber(153))
+}
