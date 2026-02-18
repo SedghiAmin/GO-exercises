febb21b4048b8f1b9908a357591ed1b9f55967c3
Amin Sedghi <sedghi.amin@gmail.com>
2026-02-18 09:42:01 +0330

    implement Sieve Prime

---
+++ Exercism/Sieve Prime/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func Sieve(limit int) []int {
+	marked := make(map[int]bool, limit)
+	prime := make([]int, 0)
+	for i := 2; i <= limit; i++ {
+		if !marked[i] {
+			prime = append(prime, i)
+			for j := i * i; j <= limit; j += i {
+				marked[j] = true
+			}
+		}
+	}
+	return prime
+}
+
+func main() {
+	fmt.Println(Sieve(1000))
+}
