9a36c9f8e46e2d1f7dba9c11c4900930132d3bf3
Amin Sedghi <sedghi.amin@gmail.com>
2026-02-22 12:27:43 +0330

    define Abbreviate func

---
+++ Exercism/Acronym/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"strings"
+)
+
+func Abbreviate(s string) string {
+	out := strings.Builder{}
+	for i := 0; i < len(s); i++ {
+		if i == 0 && string(s[i]) != " " && string(s[i]) != "-" && string(s[i]) != "_" {
+			out.WriteString(string(s[i]))
+			continue
+		}
+		if string(s[i]) == " " || string(s[i]) == "-" || string(s[i]) == "_" {
+			if (i+1 < len(s)) && (string(s[i+1]) != " ") && (string(s[i+1]) != "-") && (string(s[i+1]) != "_") {
+				out.WriteString(strings.ToUpper(string(s[i+1])))
+			}
+		}
+	}
+	return out.String()
+}
