88753f5401789e48f11288965293f0ad60f95653
Amin Sedghi <sedghi.amin@gmail.com>
2026-02-24 12:42:27 +0330

    define numToWord func

---
+++ Exercism/Bottle Song/main.go
@@ -0,0 +1,27 @@
+package main
+
+func numToWord(num int) string {
+	switch num {
+	case 1:
+		return "one green bottle"
+	case 2:
+		return "two green bottles"
+	case 3:
+		return "three green bottles"
+	case 4:
+		return "four green bottles"
+	case 5:
+		return "five green bottles"
+	case 6:
+		return "six green bottles"
+	case 7:
+		return "seven green bottles"
+	case 8:
+		return "eight green bottles"
+	case 9:
+		return "nine green bottles"
+	case 10:
+		return "ten green bottles"
+	}
+	return ""
+}
