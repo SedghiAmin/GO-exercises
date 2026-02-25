3ce805f2243c7810984ea0707503c8df09485d08
Amin Sedghi <sedghi.amin@gmail.com>
2026-02-25 11:24:40 +0330

    compelete Verse func

--- Exercism/Twelve Days/main.go
+++ Exercism/Twelve Days/main.go
@@ -19,4 +19,20 @@ func Verse(i int) string {
 		"seven Swans-a-Swimming", "eight Maids-a-Milking", "nine Ladies Dancing",
 		"ten Lords-a-Leaping", "eleven Pipers Piping", "twelve Drummers Drumming",
 	}
+
+	result := strings.Builder{}
+	tmp := fmt.Sprintf("On the %v day of Christmas my true love gave to me: ", days[i-1])
+	result.WriteString(tmp)
+	for j := i - 1; j >= 0; j-- {
+		tmp = fmt.Sprintf(gifts[j])
+		result.WriteString(tmp)
+		if j-1 == 0 {
+			result.WriteString(", and ")
+		}
+		if j-1 > 0 {
+			result.WriteString(", ")
+		}
+	}
+	result.WriteString(".")
+	return result.String()
 }
