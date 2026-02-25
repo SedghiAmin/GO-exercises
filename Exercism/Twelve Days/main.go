e7afa43b018eab47e5f9938e390df08347070622
Amin Sedghi <sedghi.amin@gmail.com>
2026-02-25 11:23:54 +0330

    define days and gifts slices

---
+++ Exercism/Twelve Days/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func Verse(i int) string {
+	if i < 1 {
+		return ""
+	}
+	days := []string{
+		"first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth", "thirteenth", "fourteenth",
+	}
+
+	gifts := []string{
+		"a Partridge in a Pear Tree", "two Turtle Doves", "three French Hens",
+		"four Calling Birds", "five Gold Rings", "six Geese-a-Laying",
+		"seven Swans-a-Swimming", "eight Maids-a-Milking", "nine Ladies Dancing",
+		"ten Lords-a-Leaping", "eleven Pipers Piping", "twelve Drummers Drumming",
+	}
+}
